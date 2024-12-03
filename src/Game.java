f5f92a9499348c468d95aed095198c04980fa635
Володимир Степанов <drugarybna@MacBook-Air-Volodimir.local>
2024-12-03 20:01:42 +0200

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,26 @@
+/*
+ * File: Game.java
+ * -------------------
+ * Author: Volodymyr "drugarybna" Stepanov
+ *
+ * This file will eventually implement the game of Bubble Shooter.
+ */
+
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+
+import javax.sound.sampled.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+
+/** Bubble Shooter Game Program */
+public class Game extends GraphicsProgram {
+
+    /* Runs the Bubble Shooter */
+    public void run() {
+
+    }
+
+}
