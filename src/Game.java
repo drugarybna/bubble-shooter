be11abcfcdd18fb8ea788de0bcd6785b10b3f235
Volodymyr Stepanov <appelsene.design@gmail.com>
2024-12-04 12:26:05 +0200

    Step 1.1: Setting the game window

--- src/Game.java
+++ src/Game.java
@@ -18,9 +18,25 @@ import java.io.*;
 /** Bubble Shooter Game Program */
 public class Game extends GraphicsProgram {
 
+    public final int APPLICATION_WIDTH = 400;
+    public final int APPLICATION_HEIGHT = 600;
+
     /* Runs the Bubble Shooter */
     public void run() {
+        setup();
+        build();
+    }
 
+    private void build() {
+
+    }
+
+    private void setup() {
+        this.setTitle("Bubble Shooter v1.0");
+        this.setSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
+        this.setFont(new Font("Arial", Font.PLAIN, 16));
+        GImage bg = new GImage("bg.png");
+        add(bg);
     }
 
 }
