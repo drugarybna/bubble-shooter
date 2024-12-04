7ee8695fd7a3c2a83b224f66a7875cdb06e9a204
Volodymyr Stepanov <appelsene.design@gmail.com>
2024-12-04 20:20:08 +0200

    Step 1.4: Adding randomized balls to the game board;

--- src/Game.java
+++ src/Game.java
@@ -8,12 +8,9 @@
 
 import acm.graphics.*;
 import acm.program.*;
-import acm.util.*;
+import acm.util.RandomGenerator;
 
-import javax.sound.sampled.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.io.*;
 
 /** Bubble Shooter Game Program */
 public class Game extends GraphicsProgram {
@@ -21,36 +18,63 @@ public class Game extends GraphicsProgram {
     public final int APPLICATION_WIDTH = 400;
     public final int APPLICATION_HEIGHT = 600;
 
-    public final int BULLET_RADIUS = 18;
+    public final int BALL_RADIUS = 20;
 
-    /* Runs the Bubble Shooter */
+    public final int BALLS_PER_ROW = 10;
+    public final int ROWS_ON_START = 3;
+
+    /** Runs the Bubble Shooter */
     public void run() {
         setup();
         build();
     }
-
+    /** Builds the game board from 0 */
     private void build() {
         addGun();
-        generateBullet();
+        generateBalls();
     }
-
-    private void generateBullet() {
-        bullet = new GOval(BULLET_RADIUS*2, BULLET_RADIUS*2);
-        bullet.setFilled(true);
-        bullet.setColor(Color.red);
-        double x = gun.getX() + gun.getWidth()/2 - bullet.getWidth()/2;
-        double y = gun.getY() - bullet.getHeight()/2;
-        add(bullet, x, y);
-        bullet.sendBackward();
+    /** Adds randomized balls to the game board */
+    private void generateBalls() {
+        for (int i = 0; i < ROWS_ON_START; i++) {
+            for (int j = 0; j < BALLS_PER_ROW; j++) {
+                double x = j * BALL_RADIUS*2;
+                double y = i * BALL_RADIUS*2;
+                ball = new GOval(x, y, BALL_RADIUS*2, BALL_RADIUS*2);
+                ball.setFilled(true);
+                ball.setColor(generateColor());
+                add(ball);
+            }
+        }
     }
-
+    /** Generates random color among the 4 colors */
+    private Color generateColor() {
+        int colIndex = rgen.nextInt(1, 4);
+        return switch (colIndex) {
+            case 1 -> Color.red;
+            case 2 -> Color.green;
+            case 3 -> Color.blue;
+            default -> Color.yellow;
+        };
+    }
+    /** Adds the gun to the game board */
     private void addGun() {
         gun = new GImage("gun.png");
         double x = (double)APPLICATION_WIDTH/2 - gun.getWidth()/2;
         double y = APPLICATION_HEIGHT - gun.getHeight()/2;
         add(gun, x, y);
+        generateBullet();
     }
-
+    /** Generates the bullet at the rear of the gun */
+    private void generateBullet() {
+        bullet = new GOval(BALL_RADIUS *2, BALL_RADIUS *2);
+        bullet.setFilled(true);
+        bullet.setColor(generateColor());
+        double x = gun.getX() + gun.getWidth()/2 - bullet.getWidth()/2;
+        double y = gun.getY() - bullet.getHeight()/2;
+        add(bullet, x, y);
+        bullet.sendBackward();
+    }
+    /** Setups the initial game config */
     private void setup() {
         this.setTitle("Bubble Shooter v1.0");
         this.setSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
@@ -59,7 +83,11 @@ public class Game extends GraphicsProgram {
         add(bg);
     }
 
+    private final RandomGenerator rgen = RandomGenerator.getInstance();
+
+    // Main objects of the game
     GImage gun;
     GOval bullet;
+    GOval ball;
 
 }
